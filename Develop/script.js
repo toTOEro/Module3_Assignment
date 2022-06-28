b36b07f2d93e24c93059de30d04e61cfff57e22b
anthony3738-miZJgd <anthony3738@gmail.com>
2022-06-27 21:45:41 -0700

    Updated with prompts for symbols/numbers

--- Develop/script.js
+++ Develop/script.js
@@ -6,17 +6,34 @@
 function generatePassword() {
   console.log('Password Generating...');
   // Initializing Character Pool, password variable, and defining length  
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var numbers = '0123456789'
+  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   var pw = '';
   var length = characters.length;
-  pwlength = window.prompt('How many characters should this be?');
+  pwlength = window.prompt('How many characters should this be?\n This must be at least 8 characters, no more than 128 characters.');
 
-  if (pwlength
+    while (pwlength < 8 || pwlength > 128) {
+      window.alert('Choose a value between 8 and 128')
+      pwlength = window.prompt('How many characters should this be?\n This must be at least 8 characters, no more than 128 characters.');
+    };
+
+    symAnswer = window.confirm('Do you want symbols?');
+    numAnswer = window.confirm('Do you want numbers?');
+
+    if (symAnswer == true) {
+      characters = characters.concat(symbols)
+    };
+
+    if (numAnswer == true) {
+      characters = characters.concat(numbers)
+    };
+
 
     for (let i = 0; i < pwlength; i++) {
       pw = pw + characters.charAt(Math.floor(Math.random() * length));
       
-    }
+    };
   return pw;
 }
 
