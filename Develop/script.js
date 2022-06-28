98053c0f2c112e2383a2d8e1236d477d2ea72b80
anthony3738-miZJgd <anthony3738@gmail.com>
2022-06-27 22:41:10 -0700

    Configured queries as functions, set while loops to ensure user configures properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,5 @@
 // Assignment code here
 
-
-
-
 function generatePassword() {
   // Initializing variables
   var characters = "";
@@ -11,52 +8,65 @@ function generatePassword() {
   var numbers = "0123456789"
   var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   var pw = '';
+  var pwlength = '';
+  var specialAnswer = false;
+  var numAnswer = false;
+  var lowerAnswer = false;
+  var upperAnswer = false;
 
+  // Function to prompt user to configure password
   function pwConfig() {
     specialAnswer = window.confirm('Do you want special characters?');
     numAnswer = window.confirm('Do you want numbers?');
     lowerAnswer = window.confirm('Do you want lower case letters?');
     upperAnswer = window.confirm('Do you want upper case letters?');
+
+    // Pending user answers, the following if statements add to the character pool for password generation
+    if (specialAnswer == true) {
+      characters = characters.concat(special)
+    };
+
+    if (numAnswer == true) {
+      characters = characters.concat(numbers)
+    };
+
+    if (lowerAnswer == true) {
+      characters = characters.concat(lower)
+    };
+
+    if (upperAnswer == true) {
+      characters = characters.concat(upper)
+    };
   }
 
-  pwlength = window.prompt('How many characters should this be?\n This must be at least 8 characters, no more than 128 characters.');
-
-  while (pwlength < 8 || pwlength > 128) {
-    window.alert('Choose a value between 8 and 128')
-    pwlength = window.prompt('How many characters should this be?\n This must be at least 8 characters, no more than 128 characters.');
+  // Function to query for length
+  function lengthQuery() {
+    pwlength = window.prompt('How many characters should this be?\nThis must be at least 8 characters, no more than 128 characters.');
   };
 
-  pwConfig()
+
+  // While loop to query for password length
+  while (pwlength < 8 || pwlength > 128) {
+    lengthQuery();
+    if (pwlength < 8 || pwlength > 128) {
+      window.alert('Choose a value between 8 and 128')
+    }
+  };
+
+
+  // While loop to query for password configuration
   
   while (specialAnswer == false && numAnswer == false && lowerAnswer == false && upperAnswer == false) {
-    window.alert('You must choose yes to one of the prompts!');
     pwConfig();
-  }
 
-  if (specialAnswer == true) {
-    characters = characters.concat(special)
-    console.log(characters)
+    if (specialAnswer == false && numAnswer == false && lowerAnswer == false && upperAnswer == false) {
+      window.alert('You must choose ok to one of the prompts!');
+    }  
   };
 
-  if (numAnswer == true) {
-    characters = characters.concat(numbers)
-    console.log(characters)
-  };
 
-  if (lowerAnswer == true) {
-    characters = characters.concat(lower)
-    console.log(characters)
-  };
-
-  if (upperAnswer == true) {
-    characters = characters.concat(upper)
-    console.log(characters)
-  };
-
+  // Generates the password based off the character pool formed in lengthQuery()
   for (let i = 0; i < pwlength; i++) {
-    // console.log(characters)
-    console.log(pwlength)
-    console.log(pw)
     pw = pw + characters.charAt(Math.floor(Math.random() * characters.length));
   };
   return pw;
